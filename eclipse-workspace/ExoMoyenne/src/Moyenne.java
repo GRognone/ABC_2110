e139cfb78765dadb2673cc4f6b9b1a95b3be6cc1
GRognone <65392789+GRognone@users.noreply.github.com>
2021-12-01 09:24:03 +0100

    today work

---
+++ eclipse-workspace/ExoMoyenne/src/Moyenne.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Moyenne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Traduction du pseudo code Java
+		
+		// DECLARATION DES VARIABLES
+		
+		//Réel nombre1
+		float nombre1;
+		//Réel nombre2
+		float nombre2;
+		//Réel moyenne
+		float moyenne;
+		
+		//SAISIE
+		//ecrire "Veuillez-saisir le 1er nombnre:"
+		Systeme.out.println("Veuillez-saisir le 1er nombre:");
+		//lire nombre1
+		Scanner sc = new Scanner(system.in);//creer un outil scanner
+		nombre1 = sc.nextFloat();
+		System.out.println("Vous avez saisie nombre1="+nombre1)
+	}
+
+}
