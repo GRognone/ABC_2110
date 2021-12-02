e2c77afddc7f87c762a8e23a7b2a4cdedc83c20d
GRognone <65392789+Jayson-Bron@users.noreply.github.com>
2021-12-02 16:55:25 +0100

    Exo du jour

---
+++ eclipse-workspace/InversiondeBloc/src/InversiondeBloc.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class InversiondeBloc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Exercice Inversion De Bloc
+		
+		//DECLARATION DES VARIABLES
+		
+		//Reel nombrea
+		float nombrea;
+		//Reel nombreb
+		float nombreb;
+		//Reel temp
+		float temp;
+		
+		//SAISIE
+		
+		//ecrire "Veuillez saisir le nombre a"
+		System.out.println("Veuillez-saisir le nombre a");
+		//lire nombrea
+		Scanner sc = new Scanner(System.in);//creer un outil scanner
+		nombrea = sc.nextFloat();
+		
+		//ecrire "Veuillez saisire le nombre b"
+		System.out.println("Veuillez-saisir le nombre b");
+		//lire nombreb
+		nombreb = sc.nextFloat();
+		
+		//TRAITEMENT
+		//temp < - - nombrea
+		temp = nombrea;
+		//nombrea < - - nombreb
+		nombrea = nombreb;
+		//nombreb < - -temp
+		nombreb = temp;
+		
+		//AFFICHAGE
+		//ecrire " Le nombre a est :"
+		System.out.println("Le nombre a est :"+nombrea);
+		//ecrire "Le nombre b est :"
+		System.out.println("le nombre b est :"+nombreb);
+	}
+
+}
