2a1df70e1c7cfcc81767c070e0a2e98e490350ea
GRognone <65392789+GRognone@users.noreply.github.com>
2021-12-07 19:24:20 +0100

    send

--- eclipse-workspace/CalculBeneficeInteret/src/CalculBeneficeIntere.java
+++ eclipse-workspace/CalculBeneficeInteret/src/CalculBeneficeIntere.java
@@ -26,31 +26,31 @@ public class CalculBeneficeIntere {
 		//lire sommeinitiales
 		Scanner  sc= new Scanner(System.in);//creer un scanner
 		sommeinitiales = sc.nextFloat();
-		System.out.println("Vous avez saisi la somme initiale"+sommeinitiales);
+		System.out.println("Vous avez saisi la somme initiale "+sommeinitiales+" €");
 		//ecrire "Veuillez-saisir le taux d'interets annuel"
 		System.out.println("Veuillez saisir le taux d'interets annuel");
 		//lire interesi
 		interesi = sc.nextFloat();
-		System.out.println("Vous avez saisi le taux d'interets annuel"+interesi);
-		//ecrire " Veulliez saisir le nombre d'ann�e"
+		System.out.println("Vous avez saisi le taux d'interets annuel "+interesi+" %");
+		//ecrire " Veulliez saisir le nombre d'annee"
 		System.out.println("Veuillez-saisir le nombre d'annee");
 		//lire nombreanneesn
 		nombreanneesn = sc.nextFloat();
-		System.out.println("Vous avez saisie le nombre d'annee"+nombreanneesn);
+		System.out.println("Vous avez saisie le nombre d'annee "+nombreanneesn);
 		
 		//TRAITEMENT
 		// valeuracquisesimple <- - sommeinitiales*(1+nombreanneesn*interesi)
-			valeuracquisesimple = (double) (sommeinitiales*(1+nombreanneesn*interesi));
+			valeuracquisesimple = (double) (sommeinitiales*(1+nombreanneesn*interesi/100));
 		//valeuracquisecomposee < - - sommeinitiales*(Math.pow((1+interesi),nombreanneesn))
-			valeuracquisecomposee = (double)(sommeinitiales*(Math.pow((1+interesi),nombreanneesn)));
+			valeuracquisecomposee = (double)(sommeinitiales*(Math.pow((1+interesi/100),nombreanneesn)));
 			
 		//AFFICHAGE
 			DecimalFormat df = new DecimalFormat("0.00");
 			
 		//ecrire "La valeur acquise simple est de :"
-		System.out.println("La valeur acquise simple est de :"+df.format(valeuracquisesimple));	
+		System.out.println("La valeur acquise simple est de :"+df.format(valeuracquisesimple)+" €");	
 		//ecrire "La valeur acquise composee est de :"
-		System.out.println("La valeur acquise composee est de :"+df.format(valeuracquisecomposee));
+		System.out.println("La valeur acquise composee est de :"+df.format(valeuracquisecomposee)+" €");
 		
 		//FIN
 	}
