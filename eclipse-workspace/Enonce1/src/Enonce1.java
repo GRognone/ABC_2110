7e9c2fab97103d0e41391c595f48d81c87bd958f
GRognone <65392789+Jayson-Bron@users.noreply.github.com>
2021-12-16 15:14:02 +0100

    send

--- eclipse-workspace/Enonce1/src/Enonce1.java
+++ eclipse-workspace/Enonce1/src/Enonce1.java
@@ -4,16 +4,15 @@ public class Enonce1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		//Enonce1
-		
+
 		//DECLARATION
-		
+
 		//Chaine compris
 		String compris;
 		//Chaine compris2
 		String compris2;
-		
+
 		//Saisie
 		//ecrire"Effectuez une lecture attentive de l'enonce"
 		System.out.println("Effectuez une lecture attentive de l'enonce");
@@ -22,43 +21,42 @@ public class Enonce1 {
 		//lire compris
 		Scanner sc = new Scanner (System.in);//creer un scanner
 		compris = sc.next();
-		compris2 = sc.next();
-		
+
+
+
 		//TRAITEMENT
-		compris = "oui";
-		
-		//Si	(compris = ="oui")
-				if (compris == "oui") {
-				
+
+
+		//Si			(compris = = "non")
+		if (compris.equals("non")) {
+
+
+			//ecrire" Relisez l'enonce de nouveau"
+			System.out.println("Effectuez une relecture de l'enonce");
+			//ecrire"Avez-vous compris l'enonce"
+			System.out.println("Avez-vous compris l'enonce cette fois ci ? oui/non ");
+			//lire compris2
+			compris2 = sc.next();
 			//FinSi
-			//Si			(compris = = "non")
-						if (compris == "non") {
-						}		
-						
-							//ecrire" Relisez l'enonce de nouveau"
-							System.out.println("Effectuez une relecture de l'enonce");
-							//ecrire"Avez-vous compris l'enonce"
-							System.out.println("Avez-vous compris l'enonce cette fois ci ? oui/non ");
-							//lire compris2
-							//FinSi
-							
-							
-							
-						 		//Si		(compris2 = = "non")
-										if (compris2 =="non"){
-										}
-										
-												//ecrire" Demandez des renseignements au formateur"
-												System.out.println("Demandez des renseignements au formateur");
-												//FinSi
-									
-	// ecrire " Effectuez le travail demandé"
-	System.out.println("Effectuez le travail demande");									
+			
+					//Si		(compris2 = = "non")
+					if(compris2.equals("non")) {
+
+						//ecrire" Demandez des renseignements au formateur"
+						System.out.println("Demandez des renseignements au formateur");
+						//FinSi
+					}
+		}
+					
+		// ecrire " Effectuez le travail demande"
+		System.out.println("Effectuez le travail demande");									
 	}
 }
-}
-										
-							
-	
+
+
+
+
+
+
 
 
