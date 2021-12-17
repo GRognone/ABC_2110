4e76cc44c75a2c28eff6587040339eac05574234
GRognone <65392789+GRognone@users.noreply.github.com>
2021-12-17 20:27:15 +0100

    send

--- eclipse-workspace/Enonce2/src/Enonce2.java
+++ eclipse-workspace/Enonce2/src/Enonce2.java
@@ -5,98 +5,109 @@ public class Enonce2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//DECLARATION
-		//Chaine beautemps
+		// DECLARATION
+		// Chaine beautemps
 		String beautemps;
-		//Chaine bicyclette fonctionne
+		// Chaine bicyclette fonctionne
 		String bicyclettefonctionne;
-		//Chaine reparation immediate
+		// Chaine reparation immediate
 		String reparationimmediate;
-		//Chaine bovarysalon
-		String bovarysalon = "";
-		//Chaine bovarymunicipale
-		String bovarymunicipale = "";
+		// Chaine bovarysalon
+		String bovarysalon;
+		// Chaine bovarymunicipale
+		String bovarymunicipale;
 
-		//SAISIE
+		// SAISIE
 
-		//ecrire "S'il fait beau demain, j'irai faire une balade à bicyclette."
-		System.out.println("S'il fait beau demain, j'irai faire une balade à bicyclette.");
-		//ecrire "Fera-t-il beau?"
+		// ecrire "S'il fait beau demain, j'irai faire une balade a bicyclette."
+		System.out.println("S'il fait beau demain, j'irai faire une balade a bicyclette.");
+		// ecrire "Fera-t-il beau?"
 		System.out.println("Fera-t-il beau? oui/non");
-		Scanner sc = new Scanner (System.in);//creer un scanner
+		Scanner sc = new Scanner(System.in);// creer un scanner
 		beautemps = sc.next();
-		
-		//TRAITEMENT
-/*
-		//Si beautemps("oui")
+
+		// TRAITEMENT
+
+		// Si beautemps("oui")
 		if (beautemps.equals("oui")) {
-			//ecrire "Je n'ai pas utilisé ma bicyclette depuis plusieurs mois, peut-être n'est-elle plus en parfait état de fonctionnement."
-			System.out.println("Je n'ai pas utilisé ma bicyclette depuis plusieurs mois, peut-être n'est-elle plus en parfait état de fonctionnement.");
-			//ecrire "Ma bicyclette est-elle en parfait etat de fonctionnement?"
+			// ecrire "Je n'ai pas utilise ma bicyclette depuis plusieurs mois, peut-etre
+			// n'est-elle plus en parfait etat de fonctionnement."
+			System.out.println("Je n'ai pas utilise ma bicyclette depuis plusieurs mois, peut-etre n'est-elle plus en parfait etat de fonctionnement.");
+			// ecrire "Ma bicyclette est-elle en parfait etat de fonctionnement?"
 			System.out.println("Ma bicyclette est-elle en parfait etat de fonctionnement? oui/non");
 			bicyclettefonctionne = sc.next();
-			//Si bicyclettefonctionne ("non")
+
+			// Si bicyclettefonctionne ("non")
 			if (bicyclettefonctionne.equals("non")) {
-				//ecrire "Si c'est le cas, je passerai chez le garagiste avant la balade."
+				// ecrire "Si c'est le cas, je passerai chez le garagiste avant la balade."
 				System.out.println("Si c'est le cas, je passerai chez le garagiste avant la balade.");
-				//ecrire "J'espère que les réparations seront immédiates sinon je devrai renoncer à la balade en bicyclette."
-				System.out.println("J'espère que les réparations seront immédiates sinon je devrai renoncer à la balade en bicyclette.");
-				//ecrire " Les reparations peuvent-elle etre immediates? oui/non"
+				// ecrire "J'espere que les reparations seront immediates sinon je devrai
+				// renoncer a la balade en bicyclette."
+				System.out.println(
+						"J'espere que les reparations seront immediates sinon je devrai renoncer a la balade en bicyclette.");
+				// ecrire " Les reparations peuvent-elle etre immediates? oui/non"
 				System.out.println(" Les reparations peuvent-elle etre immediates? oui/non");
-				reparationimmediate = sc.next();
-				//Si reparationimmediate ("non")
-				if (reparationimmediate.equals("non")) {
-					//ecrire "Comme je veux de toute façon profiter du beau temps, si mon vélo n'est pas utilisable, j'irai à pied jusqu'à l'étang pour cueillir les joncs.
-					System.out.println("Comme je veux de toute façon profiter du beau temps, si mon vélo n'est pas utilisable, j'irai à pied jusqu'à l'étang pour cueillir les joncs."
-										);		
-				
-					else  {
-					//ecrire "J'irais faire de la bicyclette."
-					System.out.println("J'irais faire de la bicyclette.");
-					}
-				//FinSi
-				}
-				else {
-				//ecrire "J'irais faire de la bicyclette."
+			}
+			
+			else {
+				// ecrire "J'irais faire de la bicyclette."
 				System.out.println("J'irais faire de la bicyclette.");
 				}
-			//FinSi
-			}
-		}	
-*/
-		
-			//Si beautemps("non")
-			if (beautemps.equals("non")) {
-			System.out.println("Il ne fait pas beau, je consacre ma journée à la lecture. J'aimerais relire Madame Bovary,  j'espère retrouver ce livre, il doit être dans la bibliothèque du salon.");
-			//ecrire "Le livre Madame Bovary est-il dans le salon? oui/non"
-			System.out.println("Le livre Madame Bovary est-il dans le salon? oui/non");
-			//Si bovarysalon("non")
-				bovarysalon = sc.next();
-				if (bovarysalon.equals("non")) {
-				//ecrire "Je ne le retrouve pas, je vais à la bibliothèque municipale pour le trouver."
-				System.out.println("Je ne le retrouve pas, je vais à la bibliothèque municipale pour le trouver.");
-				System.out.println("Le livre Madame Bovary est-il disponible à la bibliotheque municipale? oui/non");
-				//Si bovarymunicipale("non")
-					bovarymunicipale=sc.next();
-					if  (bovarymunicipale.equals("non")) {
-					//ecrire "Le livre Madame Bovary n'est pas disponible, j'emprunte un bouquin policier, je rentre ensuite directement à la maison."
-					System.out.println("Le livre Madame Bovary n'est pas disponible,j'emprunte un bouquin policier, je rentre ensuite directement à la maison.");
+			
+				reparationimmediate = sc.next();
+				// Si reparationimmediate ("non")
+				if (reparationimmediate.equals("non")) {
+					// ecrire "Comme je veux de toute facon profiter du beau temps, si mon velo
+					// n'est pas utilisable, j'irai a pied jusqu'a l'etang pour cueillir les joncs.
+					System.out.println(
+							"Comme je veux de toute facon profiter du beau temps, si mon velo n'est pas utilisable, j'irai a pied jusqu'a l'etang pour cueillir les joncs.");
 					}
+					
+				else {
+					// ecrire "J'irais faire de la bicyclette."
+					System.out.println("J'irais faire de la bicyclette.");
 				}
-		}
-	
-				// ecrire "Dès que j'aurai le livre qui me convient, je m'installerai confortablement dans un fauteuil et je me plongerai dans la lecture."
-				System.out.println("Dès que j'aurai le livre qui me convient, je m'installerai confortablement dans un fauteuil et je me plongerai dans la lecture.");
-	}
-}
-
 				
-		
-/*
-  */
- 
-		
+				// FinSi	
+			}
+			//FinSi
 	
+		else {
 
+		// Si beautemps("non")
+		if (beautemps.equals("non")) {
+			System.out.println(
+					"Il ne fait pas beau, je consacre ma journee a la lecture. J'aimerais relire Madame Bovary,  j'espere retrouver ce livre, il doit etre dans la bibliotheque du salon.");
+			// ecrire "Le livre Madame Bovary est-il dans le salon? oui/non"
+				System.out.println("Le livre Madame Bovary est-il dans le salon? oui/non");
+				bovarysalon = sc.next();
+			
+			// Si Bovarysalon("non")
+			if (bovarysalon.equals("non")) {
+				// ecrire "Je ne le retrouve pas, je vais a la bibliotheque municipale pour le
+				// trouver."
+				System.out.println("Je ne le retrouve pas, je vais a la bibliotheque municipale pour le trouver.");
+				System.out.println("Le livre Madame Bovary est-il disponible a la bibliotheque municipale? oui/non");
+				bovarymunicipale = sc.next();
+				
+				// Si bovarymunicipale("non")
+				if (bovarymunicipale.equals("non")) {
+					// ecrire "Le livre Madame Bovary n'est pas disponible, j'emprunte un bouquin
+					// policier, je rentre ensuite directement a la maison."
+					System.out.println("Le livre Madame Bovary n'est pas disponible,j'emprunte un bouquin policier, je rentre ensuite directement a la maison.");
+					
+				}
+				// FinSi
+				
+			}
+			// FinSI
+			System.out.println("Je m'installe confortablement dans un fauteuil et je me plonge dans la lecture.");
+		}
+		// FinSi
+		}
+		//FinSi
+		}
+	}
 
+		
 
