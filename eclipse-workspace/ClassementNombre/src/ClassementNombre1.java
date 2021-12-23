0f5d0a09a83d4fd2928d52b10b5ac0dd7c893232
GRognone <65392789+Jayson-Bron@users.noreply.github.com>
2021-12-23 10:56:48 +0100

    test

--- eclipse-workspace/ClassementNombre/src/ClassementNombre1.java
+++ eclipse-workspace/ClassementNombre/src/ClassementNombre1.java
@@ -4,28 +4,42 @@ public class ClassementNombre1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+
+
+		//DECLARATION
+
+		//reel = nombrea
+		//reel = nombreb
+		int nombrea;
+		int nombreb;
+		Scanner sc= new Scanner (System.in);// creation d'un scanner
+
+		//SAISIE 
+		//ecrire (" Veuillez-saisir le nombre a")
+		System.out.println("Veuillez-saisir ne nombre a");
+		//lire nombrea
+		nombrea=sc.nextInt();
+
+		//ecrire ("veuillez-saisir le nombre b ")
+		System.out.println("Veuillez-saisir ne nombre b");
+		//lire nombreb
+		nombreb=sc.nextInt();
+
+		//TRAITEMENT
+		if(nombrea >nombreb){
+			System.out.println("a>b");
+		}
+		else if(nombrea<nombreb){
+			System.out.println("a<b");
+		}
+		else {
+			System.out.println("a=b");
+		}
 		
-
-
-//DECLARATION
-
-//reel = nombrea
-//reel = nombreb
-int nombrea;
-int nombreb;
-
-//SAISIE 
-
-//ecrire (" Veuillez-saisir le nombre a")
-System.out.println("Veuillez-saisir ne nombre a");
-//lire nombrea
-nombrea= sc.next() ;
-//ecrire ("veuillez-saisir le nombre b ")
-System.out.println("Veuillez-saisir ne nombre b");
-//lire nombreb
-nombreb = sc.next();
-Scanner sc= new Scanner (System.in);// creation d'un scanner
-sc.close();
+		sc.close();
 	}
 
 }
+
+
