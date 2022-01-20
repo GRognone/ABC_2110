c1bed1573af53f90989b67062cf31320ef2322ed
GRognone <65392789+GRognone@users.noreply.github.com>
2022-01-20 22:20:51 +0100

    avancée

--- eclipse-workspace/ListerLesNombresParfaits/src/Listerlesnombresparfait.java
+++ eclipse-workspace/ListerLesNombresParfaits/src/Listerlesnombresparfait.java
@@ -6,28 +6,36 @@ public class Listerlesnombresparfait {
 		// TODO Auto-generated method stub
 		Scanner  sc = new Scanner(System.in);
 		int combien;
-		long nombre;
+		long nombreparfait;
+
+
+		long nombre=0;
 		long diviseur;
 		boolean pasdautrequeN = false;
 		long sommediviseurs = 0;
 
+
 		System.out.println("Combien de nombre parfait souhaitez-vous  afficher s'il vous plait?");
 		combien=sc.nextInt();
+		for (int i=0;i<combien;i++) {
 
 
-		nombre = sc.nextLong();
-		for (diviseur = 1;diviseur<(nombre-1);diviseur++) {
-			if (nombre%diviseur==0){
-				System.out.print(diviseur+ " . ");
-				sommediviseurs += diviseur;
-				pasdautrequeN = true;			
-			}	
+			// calcul des nombres parfait
+
+
+			for (diviseur = 1;diviseur<(nombre-1);diviseur++) {
+				if (nombre%diviseur==0){
+					sommediviseurs += diviseur;
+					pasdautrequeN = true;			
+				}	
+			}
+
+			if (sommediviseurs==diviseur){
+			}
+			System.out.println(" Les "+combien+" nombres parfait sont:");
+			System.out.println(/*nombreparfait+*/ " " );
 		}
-		System.out.println("La somme des diviseur est de "+sommediviseurs);
-		if (sommediviseurs==nombre){
-			System.out.println(" Le nombre " +nombre + " est un nombre parfait");
-		}
-
 		sc.close();
 	}
 }
+
