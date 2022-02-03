d2db21e2de9baa492ae11bcdcb561290b89c68c7
GRognone <65392789+Jayson-Bron@users.noreply.github.com>
2022-02-03 15:38:25 +0100

    save today

--- eclipse-workspace/Exercice2.1.2/src/Exercice212.java
+++ eclipse-workspace/Exercice2.1.2/src/Exercice212.java
@@ -5,26 +5,31 @@ public class Exercice212 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		double distanceAconvertir;
+		double distanceAConvertir;
 		double distanceConvertieEnKm;
 		double distanceConvertieEnMiles;
 		String uniteDeMesure;
 
 		System.out.println("Veuillez saisir la distance et son unite de mesure à convertir mi pour Miles ou km pour Kilometres svp");
-		distanceAconvertir = sc.nextDouble();
+		distanceAConvertir = sc.nextDouble();
 		uniteDeMesure = sc.next().toLowerCase();
-
+		do
+		{
+			System.out.println("Veuillez saisir uniquement l'unité de mesure d'origine svp");
+			uniteDeMesure = sc.next().toLowerCase();
+		} while (!uniteDeMesure.equals("mi") && !uniteDeMesure.equals("km"));
+		
 		if (uniteDeMesure.equals("mi")){
-			distanceConvertieEnKm = distanceAconvertir * 1.609;
-			System.out.println("La distance "+distanceAconvertir+" miles convertie est de" +distanceConvertieEnKm+" Km");
+			distanceConvertieEnKm = distanceAConvertir * 1.609;
+			System.out.println("La distance "+distanceAConvertir+" miles convertie est de" +distanceConvertieEnKm+" Km");
 		}
 		else if ((uniteDeMesure.equals("km"))) {
-			distanceConvertieEnMiles = distanceAconvertir / 1.609;
-			System.out.println("La distance "+distanceAconvertir+" Km convertie est de" +distanceConvertieEnMiles+" Miles");
+			distanceConvertieEnMiles = distanceAConvertir / 1.609;
+			System.out.println("La distance "+distanceAConvertir+" Km convertie est de" +distanceConvertieEnMiles+" Miles");
 		}
 		else  {
-			distanceConvertieEnMiles = distanceAconvertir / 1.609;
-			System.out.println("La distance "+distanceAconvertir+" Km convertie est de" +distanceConvertieEnMiles+" Miles");
+			distanceConvertieEnMiles = distanceAConvertir / 1.609;
+			System.out.println("La distance "+distanceAConvertir+" Km convertie est de" +distanceConvertieEnMiles+" Miles");
 		}
 		sc.close();
 	}
