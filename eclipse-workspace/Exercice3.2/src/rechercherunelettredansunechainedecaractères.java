b44840f41a40321f03f2f9928a99c18913d48fe2
GRognone <65392789+Jayson-Bron@users.noreply.github.com>
2022-02-25 11:24:07 +0100

    send befor holidays

--- eclipse-workspace/Exercice3.2/src/rechercherunelettredansunechainedecaractères.java
+++ eclipse-workspace/Exercice3.2/src/rechercherunelettredansunechainedecaractères.java
@@ -1,27 +1,57 @@
 import java.util.Scanner;
 
-public class rechercherunelettredansunechainedecaract�res {
+public class rechercherunelettredansunechainedecaractères {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner sc= new Scanner(System.in);
-		String caractere[ ];
+		String tableauCaractere[ ];
 		String saisie;
 		Boolean  vide =false;
-		
-			
-		
+		String caractereRecherche;
+		int compteur=0;
+
+
+		// Saisie du tableau
+
 		System.out.println("veuillez saisir une chaine de caractere et finir la saisie par un point");
-		saisie=sc.next();
+		saisie=sc.nextLine();
+		tableauCaractere = saisie.split (" ");
+
 		if  (saisie.equals(".")) {
-		vide=true;
-		System.out.println("Chaine vide");
+			vide=true;
+			System.out.println("Chaine vide");
 		}
 		else {
-			System.out.println(saisie);
+			// Rechercher et comptabiliser un caractere 
+
+			System.out.println("Veuillez saisir le caractère à rechercher svp");
+			caractereRecherche=sc.nextLine();
+			for (int i=0; i<tableauCaractere.length;i++){
+				if (caractereRecherche.equals(tableauCaractere[i])) {
+					compteur++;
+				}
+			}
+			if (compteur==0) {
+				System.out.println("Le caractère recherché n'existe pas");
+			}
+			else {
+				System.out.println("Le caractereRecherche "+caractereRecherche+" est disponible "+compteur+ " fois dans le tableau java");
+			}
 		}
-		
 		sc.close();
 	}
 }
+
+/*package character_manipulation;
+
+public class DeclareCharArray {
+    public static void main(String[] args) {
+        String s1 = "First String";
+        char[] charArray = s1.toCharArray();
+        for (char c : charArray) {
+            System.out.print(" " + c);
+        }
+    }
+}*/
