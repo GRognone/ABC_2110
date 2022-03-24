6f300b408c86b0bf032a93edaf131c43271e2026
GRognone <65392789+Jayson-Bron@users.noreply.github.com>
2022-03-24 15:52:24 +0100

    Save 24 03 22

--- eclipse-workspace/Exercice3.7/src/rechercheDichotomie.java
+++ eclipse-workspace/Exercice3.7/src/rechercheDichotomie.java
@@ -1,41 +1,51 @@
 import java.util.Scanner;
 
 public class rechercheDichotomie {
-/*Recherche par dichotomie d'un élément dans une table classée
-
-Nous désirons chercher l'indice de la case de la table où se trouve le prénom, si il s'y trouve.
-Pour cela, nous utiliserons la méthode de dichotomie (voir ci-dessous la méthode)
-Donnez l'algorithme de la procédure qui recherche, par dichotomie le numéro du prénom recherché ou zéro s'il n'y 
-est pas.
-
-Algorithme:
-On partitionne la table en 2 sous-tables et un élément médian, et, suivant le résultat de la comparaison de l'élément 
-médian et du prénom recherché (plus grand, plus petit ou égal) on recommence la recherche sur une des 2 soustables, jusqu'à avoir trouvé ou obtenir une sous-table vide (le prénom est alors absent de la table).
-On cherche 'olga' dans la table:
-Milieu: élément 4
-'olga'>'cunégonde' -> 'olga' est entre 4 et 7
-milieu: élément 6
-'olga' < 'raymonde' -> 'olga' est entre 4 et 6
-mileu: élément 5
-'olga' trouvé en 5
-
-*/
+	
 	public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
+		String saisieUtilisateur;
 		String[ ]monTableau;
+		boolean prenomAbsent = true;
+		
 		monTableau=new String[8];
-		monTableau[0]= null;
+		monTableau[0]= "Zero";
 		monTableau[1]="Agathe";
 		monTableau[2]="Berthe";
-		monTableau[3]="Cloé";
-		monTableau[4]="Cunégonde";
+		monTableau[3]="Cloe";
+		monTableau[4]="Cunegonde";
 		monTableau[5]="Olga";
 		monTableau[6]="Raymonde";
 		monTableau[7]="Sidonie";
 
-		for (int i=0; i<monTableau.length;i++) {
+		System.out.println("Veuillez saisir un prenom à rechercher svp en commençant par une majuscule svp.");
+		saisieUtilisateur = sc.nextLine();
+		
+		System.out.println(saisieUtilisateur);
 
+		if (saisieUtilisateur.compareTo(monTableau[4])>0) {
+			for (int i=4; i<monTableau.length;i++) {
+				if (saisieUtilisateur.compareTo(monTableau[i])==0) {
+					System.out.println("Le prenom "+saisieUtilisateur+" se trouve à l'indice "+i+" du tableau");
+					prenomAbsent = false;
+				}
+			}
+		}
+		else if (saisieUtilisateur.compareTo( monTableau[4])<0) {
+			for (int i=1; i<monTableau.length/2;i++) {
+				if ( saisieUtilisateur.compareTo(monTableau[i])==0) {
+					System.out.println("Le prenom "+saisieUtilisateur+" se trouve à l'indice  "+i+" du tableau");
+					prenomAbsent = false;
+				}
+			}
+		}
+		else if (saisieUtilisateur.compareTo(monTableau[4])==0) {
+			System.out.println("Le prenom "+saisieUtilisateur+" se trouve à l'indice "+monTableau.length/2+" du tableau");
+			prenomAbsent = false;
+		}
+		if (prenomAbsent) {
+			System.out.println("Le prenom n'existe pas dans le tableau. ");
 		}
 		sc.close();
 	}
